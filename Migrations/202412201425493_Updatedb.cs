078ab313bd5a48280f6b0eb1a9d806b50539c3b2
Manh203 <maycuame2@gmail.com>
2025-02-17 18:03:31 +0700

    a

---
+++ Migrations/202412201425493_Updatedb.cs
@@ -0,0 +1,18 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Updatedb : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.tb_Product", "IsHome", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.tb_Product", "IsHome");
+        }
+    }
+}
---
+++ Models/EF/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_Contact")]
+    public class Contact:CommonAbstract
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Tên không được để trống")]
+        [StringLength(150,ErrorMessage ="độ dài không quá 150")]
+        public string Name { get; set; }
+        [StringLength(150, ErrorMessage = "độ dài không quá 150")]
+        public string Email { get; set; }
+        
+        public string Website { get; set; }
+        [StringLength(4000)]
+        public string Message { get; set; }
+        public int IsRead { get; set; }
+        
+    }
+}
